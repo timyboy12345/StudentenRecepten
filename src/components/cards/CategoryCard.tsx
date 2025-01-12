e254e59263ebe5d6ea50c169433f74645002729c
Tim Arendsen <t.j.m.arendsen@gmail.com>
2025-01-12 13:41:05 +0100

    v0.1.2: Improved category UI

--- src/components/cards/CategoryCard.tsx
+++ src/components/cards/CategoryCard.tsx
@@ -2,6 +2,6 @@ import Card from "@/components/cards/Card";
 
 export default function CategoryCard({category}) {
     return (
-        <Card title={category.title} image={category.image} link={'/categorieen/' + category.slug}/>
+        <Card title={category.title} image={category.image} description={category.description} link={'/categorieen/' + category.slug}/>
     )
 }
--- src/pages/categorieen/[slug].tsx
+++ src/pages/categorieen/[slug].tsx
@@ -21,9 +21,10 @@ function Recipe() {
     return (
         <>
             {category.image && <DirectusImage height='h-64' image={category.image}/>}
-            <h1 className='font-serif text-2xl mb-4 mt-4'>{category.title}</h1>
+            <h1 className='font-serif text-2xl mb-2 mt-4'>{category.title}</h1>
             <p dangerouslySetInnerHTML={toHtml(category.content)} className='text-gray-600'/>
 
+            <h2 className='font-serif text-xl mt-4 mb-2'>Recepten in deze categorie</h2>
             <div className='grid grid-cols-2 lg:grid-cols-4'>
                 {category.recipes.map((object, i) => <RecipeCard key={i} recipe={object.recipes_id}/>)}
             </div>
